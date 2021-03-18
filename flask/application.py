a89dd0ba5909f4aacd5fd87ae0b722d2ac300276
Ian Littke <v-ialit@microsoft.com>
2021-03-18 16:59:09 -0700

    try redirect another time

--- flask/application.py
+++ flask/application.py
@@ -18,6 +18,17 @@ app = Flask(__name__)
 #         code = 301
 #         return redirect(url, code=code)
 
+@app.before_request
+def before_request():
+    if app.env == "development":
+        return
+    scheme = request.headers.get('X-Forwarded-Proto')
+    if scheme and scheme == 'http' and request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
