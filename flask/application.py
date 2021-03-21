98e8f0c15a1a3ea13969d261baadd174d1a68db2
Ian Littke <ian.littke@gmail.com>
2021-03-21 14:34:42 -0700

    initial rsvp email

--- flask/application.py
+++ flask/application.py
@@ -1,39 +1,38 @@
 import os.path
 import json
-from flask import Flask, send_from_directory, url_for, redirect
+from flask import Flask, send_from_directory, url_for, redirect, flash, make_response
 from flask import render_template, request
+from flask_mail import Mail, Message
 import logging
+from datetime import datetime
 
-import datetime
-
-delta = datetime.datetime(2021, 8, 21) - datetime.datetime.now()
+delta = datetime(2021, 8, 21, 13) - datetime.now()
 delta.days
 
 app = Flask(__name__)
 
-# @app.before_request
-# def before_request():
-#     if request.url.startswith('http://'):
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL'],
+    "MAIL_PASSWORD": os.environ['PASSWRD']
+}
 
-@app.before_request
-def before_request():
-    if app.env == "development":
-        return
-    scheme = request.headers.get('X-Forwarded-Proto')
-    if scheme and scheme == 'http' and request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
+app.config.update(mail_settings)
+mail = Mail(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    print('index.html')
+    print("browser time: ", request)
     return render_template('/index.html', days=delta.days)
+
+@app.route("/thankyou.html")
+def thankyou():
+    return render_template('/thankyou.html', days=delta.days)
 
 @app.route("/travel.html")
 def travel():
@@ -50,6 +49,46 @@ def photos():
 @app.route("/registry.html")
 def registry():
     return render_template('/registry.html', days=delta.days)
+
+@app.route('/rsvp.html', methods=['GET', 'POST'])
+def rsvp():
+	error = None
+	formfilled = request.cookies.get('rsvpstat')
+	if formfilled == '1':
+		return redirect(url_for('thankyou.html'))
+	if request.method == 'POST':
+		if 'inputName' in request.form:
+			if request.form['inputName'] == '':
+				error = "Name field cannot be blank."
+				return render_template('rsvp.html', error=error)
+			else:
+				inputName = request.form['inputName']
+		if 'inputGuest' in request.form:
+			inputGuest = request.form['inputGuest']
+		if 'inputEmail' in request.form:
+			inputEmail = request.form['inputEmail']
+		if 'decision' in request.form:
+			inputDecision = request.form['decision']
+		if 'inputComment' in request.form:
+			inputComment = request.form['inputComment']
+
+		with app.app_context():
+			msg = Message(subject="Wedding RSVP",
+			sender=app.config.get("MAIL_USERNAME"),
+			recipients=["the.littkes@gmail.com"],
+			body=(f"From:     {inputName}\n"
+				  f"And:      {inputGuest}\n"
+			      f"Email:    {inputEmail}\n"
+				  f"Question: {inputComment}\n"
+				  f"Response: {inputDecision}"))
+		print("Sending an email!")
+		mail.send(msg)
+
+		resp = make_response(redirect(url_for('thankyou.html')))
+		resp.set_cookie('rsvpstat', '1')
+		return resp
+	return render_template('rsvp.html', days=delta.days, error=error)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
