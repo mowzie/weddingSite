0159d79c89d553269a9583861642d6682a95e425
Ian Littke <v-ialit@microsoft.com>
2021-03-24 10:57:10 -0700

    settings test

--- flask/application.py
+++ flask/application.py
@@ -27,7 +27,7 @@ mail = Mail(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('/index.html', days=delta.days)
+    return render_template('/index.html', days=delta.days, email=app.config.get("MAIL_USERNAME"), passwrd=app.config.get("MAIL_PASSWORD"))
 
 @app.route("/thankyou.html")
 def thankyou():
