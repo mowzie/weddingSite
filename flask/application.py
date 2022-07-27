a4e3b45fa44b5690cd0609f60cce2870f84a95a3
Littke <ian.littke@gmail.com>
2022-07-27 07:30:14 -0700

    Remove old pages, show photos

--- flask/application.py
+++ flask/application.py
@@ -1,24 +1,17 @@
 import os.path
 import json
+import re
+import requests
+import sys
 from flask import Flask, send_from_directory, url_for, redirect, flash, make_response
 from flask import render_template, request
-from flask_mail import Mail, Message
 import logging
 from datetime import datetime
 
 app = Flask(__name__)
 
-mail_settings = {
-    "MAIL_SERVER": 'email-smtp.us-west-2.amazonaws.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['EMAIL'],
-    "MAIL_PASSWORD": os.environ['PASSWRD']
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
+# app.config.update(mail_settings)
+# mail = Mail(app)
 
 @app.route('/')
 @app.route('/index')
@@ -43,55 +36,63 @@ def story():
 
 @app.route("/photos.html")
 def photos():
+    link = "https://photos.google.com/share/AF1QipN-gdB9_hmJ4vS0fysQ8bzgsdSRDbk05nvWpdptW7VadGRtCzQn9MeUD_rMCkDhDA?key=ajczZEJuNDg2eWF4aVRBaHdFaGhqdG41WHp3U2dB"
+    response = requests.get(link)
+
+    pattern = "\[\"(https:\/\/lh3\.googleusercontent\.com/[a-zA-Z0-9\-_]*)\""
+
+    photoSet = set(re.findall(pattern, response.text))
+
     delta = datetime(2021, 8, 21, 13) - datetime.now()
-    return render_template('/photos.html', days=delta.days)
+    return render_template('/photos.html', days=delta.days, len=len(photoSet), photoList=photoSet)
 
-@app.route("/registry.html")
-def registry():
-    delta = datetime(2021, 8, 21, 13) - datetime.now()
-    return render_template('/registry.html', days=delta.days)
 
-@app.route('/rsvp.html', methods=['GET', 'POST'])
-def rsvp():
- delta = datetime(2021, 8, 21, 13) - datetime.now()
- error = None
- formfilled = request.cookies.get('rsvpstat')
- if formfilled == '1':
-  return redirect(url_for('thankyou'))
- if request.method == 'POST':
-  if 'inputName' in request.form:
-   if request.form['inputName'] == '':
-    error = "Name field cannot be blank."
-    return render_template('rsvp.html', days=delta.days, error=error)
-   else:
-    inputName = request.form['inputName']
-  if 'inputGuest' in request.form:
-   inputGuest = request.form['inputGuest']
-  if 'inputEmail' in request.form:
-   if request.form['inputEmail'] == '':
-    error = "Email field cannot be blank."
-    return render_template('rsvp.html', days=delta.days, error=error)
-   else:
-    inputEmail = request.form['inputEmail']
-  if 'decision' in request.form:
-   inputDecision = request.form['decision']
-  if 'inputComment' in request.form:
-   inputComment = request.form['inputComment']
+# @app.route("/registry.html")
+# def registry():
+#     delta = datetime(2021, 8, 21, 13) - datetime.now()
+#     return render_template('/registry.html', days=delta.days)
 
-  with app.app_context():
-   msg = Message(subject="Wedding RSVP",
-   sender='the.littkes@gmail.com',
-   recipients=["the.littkes@gmail.com"],
-   body=(f"From:     {inputName}\n"
-      f"And:      {inputGuest}\n"
-         f"Email:    {inputEmail}\n"
-      f"Question: {inputComment}\n"
-      f"Response: {inputDecision}"))
-  mail.send(msg)
-  resp = make_response(redirect(url_for('thankyou')))
-  resp.set_cookie('rsvpstat', '1')
-  return resp
- return render_template('rsvp.html', days=delta.days, error=error)
+# @app.route('/rsvp.html', methods=['GET', 'POST'])
+# def rsvp():
+#  delta = datetime(2021, 8, 21, 13) - datetime.now()
+#  error = None
+#  formfilled = request.cookies.get('rsvpstat')
+#  if formfilled == '1':
+#   return redirect(url_for('thankyou'))
+#  if request.method == 'POST':
+#   if 'inputName' in request.form:
+#    if request.form['inputName'] == '':
+#     error = "Name field cannot be blank."
+#     return render_template('rsvp.html', days=delta.days, error=error)
+#    else:
+#     inputName = request.form['inputName']
+#   if 'inputGuest' in request.form:
+#    inputGuest = request.form['inputGuest']
+#   if 'inputEmail' in request.form:
+#    if request.form['inputEmail'] == '':
+#     error = "Email field cannot be blank."
+#     return render_template('rsvp.html', days=delta.days, error=error)
+#    else:
+#     inputEmail = request.form['inputEmail']
+#   if 'decision' in request.form:
+#    inputDecision = request.form['decision']
+#   if 'inputComment' in request.form:
+#    inputComment = request.form['inputComment']
+
+#   with app.app_context():
+#    msg = Message(subject="Wedding RSVP",
+#    sender='the.littkes@gmail.com',
+#    recipients=["the.littkes@gmail.com"],
+#    body=(f"From:     {inputName}\n"
+#       f"And:      {inputGuest}\n"
+#          f"Email:    {inputEmail}\n"
+#       f"Question: {inputComment}\n"
+#       f"Response: {inputDecision}"))
+#   mail.send(msg)
+#   resp = make_response(redirect(url_for('thankyou')))
+#   resp.set_cookie('rsvpstat', '1')
+#   return resp
+#  return render_template('rsvp.html', days=delta.days, error=error)
 
 
 @app.errorhandler(404)
